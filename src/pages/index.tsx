37ead666500cc0ea46630fb32235abd513e74b77
Ruan Vitor Scherer <ruan.scherer@outlook.com>
2021-02-24 08:48:09 -0300

    [WIP] feat: countdown component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import Head from "next/head";
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import styles from "../styles/pages/Home.module.css"
@@ -7,12 +9,17 @@ import styles from "../styles/pages/Home.module.css"
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Home | Move.it</title>
+      </Head>
+
       <ExperienceBar/>
 
       <section>
         <div>
           <Profile/>
           <CompletedChallenges/>
+          <Countdown/>
         </div>
         
         <div>
