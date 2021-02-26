fb3da4e07bc20cadc46303fe48165b2f5a291002
Ruan Vitor Scherer <ruan.scherer@outlook.com>
2021-02-26 20:29:35 -0300

    feat: store app data using cookies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,18 @@
 import { ThemeSwitcher } from "../components/ThemeSwitcher"
 import { ApplicationProvider } from "../contexts/ApplicationContext"
-import { ChallengesProvider } from "../contexts/ChallengesContext"
 import { AppContext } from "./_appContext"
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChallengesProvider>
-      <ApplicationProvider>
-        <AppContext>
-          <>
-            <Component {...pageProps} />
-            <ThemeSwitcher/>
-          </>
-        </AppContext>
-      </ApplicationProvider>
-    </ChallengesProvider>
+    <ApplicationProvider>
+      <AppContext>
+        <>
+          <Component {...pageProps} />
+          <ThemeSwitcher/>
+        </>
+      </AppContext>
+    </ApplicationProvider>
   )
 }
 
