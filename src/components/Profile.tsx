cf3b348d517560ef157827105e9e6278add36427
Ruan Vitor Scherer <ruan.scherer@outlook.com>
2021-03-01 20:22:23 -0300

    feat: use github profile info

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,22 @@
+import { useEffect, useState } from "react"
 import { useChallenges } from "../contexts/ChallengesContext"
 import styles from "../styles/components/Profile.module.css"
 
 export function Profile() {
   const { level } = useChallenges()
+  const [avatar, setAvatar] = useState("")
+  const [name, setName] = useState("")
+
+  useEffect(() => {
+    setAvatar(localStorage.getItem("userPhoto"))
+    setName(localStorage.getItem("userName"))
+  })
 
   return (
     <div className={styles.profileContainer}>
-      <img src="https://github.com/RuanScherer.png" alt="Ruan Schere"/>
+      <img src={avatar || "https://avatars.githubusercontent.com/u/44452841?s=460&v=4"} alt={name}/>
       <div>
-        <strong>Ruan Scherer</strong>
+        <strong>{name || "Anônimo"}</strong>
         <p>
           <img src="icons/level.svg" alt="Ícone de nível"/>
           Level {level}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,29 @@
-import Head from "next/head";
-import { useState } from "react";
+import Head from "next/head"
+import { useRouter } from 'next/router'
+import { useState } from "react"
+import axios from 'axios'
 import styles from "../styles/pages/Login.module.css"
 
 export default function Login() {
   const [username, setUsername] = useState("")
+  const { replace } = useRouter()
+
+  function handleLogin() {
+    if (username) {
+      axios.get(`https://api.github.com/users/${username}`, {
+        headers: {
+          Accept: 'application/vnd.github.v3+json'
+        }
+      }).then(response => {
+        localStorage.setItem("userPhoto", response.data.avatar_url)
+        localStorage.setItem("userName", response.data.name)
+      }).catch(error => {
+        console.log(error)
+      }).finally(() => {
+        replace("/")
+      })
+    }
+  }
 
   return (
     <main className={styles.loginContainer}>
@@ -32,7 +52,7 @@ export default function Login() {
             value={username}
             onChange={event => setUsername(event.target.value)}
           />
-          <button disabled={!username}>
+          <button disabled={!username} onClick={handleLogin}>
             <img src="/icons/arrow-right.svg" alt="Avançar"/>
           </button>
         </div>
